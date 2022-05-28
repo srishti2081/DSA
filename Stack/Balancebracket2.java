bccf75f22eb7d03dcf32ab46e29199180b5fbad2
srishti2081 <btech15117.19@bitmesra.ac.in>
2022-05-28 20:23:01 +0530

    folder wise

---
+++ Stack/Balancebracket2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Balancebracket2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int n = sc.nextInt();
+        
+        Stack <Character> st=new Stack<Character>();
+        String a = sc.nextLine();
+        
+        for(int i = 0; i < a.length(); i++){
+
+        }
+        
+        
+        sc.close();
+        
+    }
+
+}
