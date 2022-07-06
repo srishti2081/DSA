320456f769036fd0588599995f841a5eb452440c
srishti2081 <btech15117.19@bitmesra.ac.in>
2022-07-07 00:51:52 +0530

    codes

--- Recursion/Allindices_ofarray.java
+++ Recursion/Allindices_ofarray.java
@@ -9,25 +9,35 @@ public class Allindices_ofarray {
             arr[i]=sc.nextInt();
         }
         int m = sc.nextInt();
-        int AI= Allindices_ofarray(arr,0,m,0);
-        System.out.println(AI);
+        int[] iarr= Allindices_ofarray(arr,m,0,0);//counter = x
+
+        if(iarr.length ==0){ //for blank Line
+            System.out.println("Not Found");
+            return;
+        }
+
+        for(int i=0;i<iarr.length;i++){
+            System.out.print(iarr[i]+" ");
+        }
         
         
         sc.close();
         
         
     }
-    public static int Allindices_ofarray(int []arr,int idx, int num ,int x) {
-        if(idx== arr.length){
-            return new int [x];
+    public static int[] Allindices_ofarray(int []arr,int num, int idx ,int x) {
+        //base case
+        if(idx == arr.length){
+            return new int[x];
         }
 
         if(arr[idx]==num){
-            Allindices_ofarray(arr,idx+1,num, x+1);
-           // return idx;
+           int[] iarr = Allindices_ofarray(arr,num,idx+1, x+1);
+           iarr[x]= idx;
+           return iarr;
         }else{
-            int a=Allindices_ofarray(arr,idx+1,num,0);
-            //return a;
+            int[] iarr=Allindices_ofarray(arr,num,idx+1,x);
+           return iarr;
         }
     }
     
